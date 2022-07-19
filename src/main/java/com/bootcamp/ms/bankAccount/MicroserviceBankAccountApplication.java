cb140d387c1e234150f819bd4ed08689edd0e771
Raul MM <raulmezamedina@gmail.com>
2022-07-19 18:22:58 -0500

    agregando CircuitBreaker

--- src/main/java/com/bootcamp/ms/bankAccount/MicroserviceBankAccountApplication.java
+++ src/main/java/com/bootcamp/ms/bankAccount/MicroserviceBankAccountApplication.java
@@ -1,14 +1,17 @@
 package com.bootcamp.ms.bankAccount;
 
+import com.bootcamp.ms.bankAccount.config.CircuitBreakerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EntityScan({"com.bootcamp.ms.commons.entity"})
+@Import({CircuitBreakerConfiguration.class})
 public class MicroserviceBankAccountApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bootcamp/ms/bankAccount/controller/BankAccountController.java
+++ src/main/java/com/bootcamp/ms/bankAccount/controller/BankAccountController.java
@@ -6,11 +6,10 @@ import com.bootcamp.ms.bankAccount.service.ProductBankService;
 import com.bootcamp.ms.commons.entity.BankAccount;
 import com.bootcamp.ms.commons.entity.ProductBank;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -39,13 +38,8 @@ public class BankAccountController {
     }
 
     @GetMapping("/allProductBank")
-    @CircuitBreaker(name = CUSTOMER_CONTACT_INFO_SERVICE, fallbackMethod = "customerContactInfoFallback")
     public Flux<ProductBank> getAllProductBank(){
         return productBankService.findAll();
-    }
-
-    public ResponseEntity<String> customerContactInfoFallback(Exception e) {
-        return new ResponseEntity<String>("GET: Customer contact info endpoint is not available right now.", HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
